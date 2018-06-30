1647c0e6cb8b127657ff29da320c44a689f98431
richie <861949775@qq.com>
2018-06-30 11:56:24 +0800

    修改中介者模式

--- mediator.cpp
+++ mediator.cpp
@@ -26,104 +26,83 @@
 */
 namespace MediatorPattern
 {
-    class Widget;
-    using Ptr_Widget = std::shared_ptr<Widget>;
     class DialogDirector;
     using Ptr_DialogDirector = std::shared_ptr<DialogDirector>;
+    class Widget;
+    using Ptr_Widget = std::shared_ptr<Widget>;
     class ListBox;
     using Ptr_ListBox = std::shared_ptr<ListBox>;
-    class EntryField;
-    using Ptr_EntryField = std::shared_ptr<EntryField>;
+    class EntityField;
+    using Ptr_EntityField = std::shared_ptr<EntityField>;
     class Button;
     using Ptr_Button = std::shared_ptr<Button>;
-    class MouseEvent;
-    using Ptr_MouseEvent = std::shared_ptr<MouseEvent>;
-    //仅仅做示范,不用实现
-    class MouseEvent
-    {
-
-    };
+    class FontDialogDirector;
+    using Ptr_FontDialogDirector = std::shared_ptr<FontDialogDirector>;
     class DialogDirector : public std::enable_shared_from_this<DialogDirector>
     {
     public:
-        virtual ~DialogDirector(){}
-        virtual  void showDialog(bool visible)
-        {
-            if (visible)
-            {
-                std::cout << "the dialog is show in front of you!" << std::endl;
-            }
-            else
-            {
-                std::cout << "the dialog is dismissed!" << std::endl;
-            }
-        }
-        virtual  void widgetChanged(Ptr_Widget) = 0;
-        virtual void createWidgets() = 0;
+        virtual ~DialogDirector(){};
+        virtual void widgetChanged(Ptr_Widget) = 0;
+        virtual void createWidget() = 0;
     };
 
     class Widget : public std::enable_shared_from_this<Widget>
     {
     public:
-        Widget(Ptr_DialogDirector director)
+        Widget(Ptr_DialogDirector directory) : m_directory(directory){}
+
+        virtual void change()//该函数用智能指针New对象后才能调用
         {
-            this->m_director = director;
+            this->m_directory->widgetChanged(this->shared_from_this());
         }
-        virtual void changed()
+        //TODO
+        virtual void handleMouse()
         {
-            this->m_director->widgetChanged(this->shared_from_this());
-        }
-        virtual void handleMouse(Ptr_MouseEvent event){
-            std::cout << "this widget do not response the click event!" << std::endl;
+            throw "you must rewrite this function!";
         }
     private:
-        Ptr_DialogDirector m_director;
+        Ptr_DialogDirector m_directory;
     };
 
-    //点击ListBox时会将内容设置到EntryField
     class ListBox : public Widget
     {
     public:
-        ListBox(Ptr_DialogDirector director) :Widget(director){}
+        ListBox(Ptr_DialogDirector directory) :Widget(directory){}
 
         std::string getSelection()
         {
-            return this->m_fontNameList[this->selectIdx];
+            return this->m_listItems[idx];
         }
 
-        void setList(std::vector<std::string>& listItems)
+        void setList(const std::vector<std::string>& listItems)
         {
-            this->m_fontNameList = listItems;
+            this->m_listItems = listItems;
         }
 
-        virtual void handleMouse(Ptr_MouseEvent event) override
+        virtual void handleMouse() override
         {
-            srand(static_cast<unsigned int>(time(nullptr)));
-            this->selectIdx = rand() % m_fontNameList.size();
-            std::cout << std::string("当前选择的是:") + this->m_fontNameList[this->selectIdx] << std::endl;
-            this->changed();
+            srand(time(nullptr));
+            this->idx = rand() % this->m_listItems.size();
+            this->change();
         }
     private:
-        std::vector<std::string> m_fontNameList;
-        std::vector<std::string>::size_type  selectIdx;
+        std::vector<std::string> m_listItems;
+        std::size_t   idx;
     };
 
-    class EntryField : public Widget
+    class EntityField : public Widget
     {
     public:
-        EntryField(Ptr_DialogDirector director) :Widget(director){}
+        EntityField(Ptr_DialogDirector directory) :Widget(directory){}
 
-        virtual void setText(const std::string& text)
-        { 
-            this->m_text = text; 
-            std::cout << std::string("EntryField txt :") + text + "\n";
-        }
+        void setText(const std::string& text){ this->m_text = text; }
 
-        virtual std::string getText()
+        std::string getText(){ return this->m_text; }
+
+        virtual void handleMouse() override
         {
-            return this->m_text;
+            std::cout << "the EntityField can not click!" << std::endl;
         }
-
     private:
         std::string m_text;
     };
@@ -131,30 +110,31 @@ namespace MediatorPattern
     class Button : public Widget
     {
     public:
-        Button(Ptr_DialogDirector director) :Widget(director){}
+        Button(Ptr_DialogDirector directory) :Widget(directory){}
 
+        /*
+            set button label
+        */
         void setText(const std::string& text)
         {
-            this->m_name = text;
+            this->m_label = text;
         }
 
-        virtual void handleMouse(Ptr_MouseEvent event)
+        virtual void handleMouse()
         {
-            std::cout << std::string("button ") + this->m_name + "is clcked" + "\n";
-            this->changed();
+            std::cout << this->m_label + " is clicked!" + "\n";
+            this->change();
         }
     private:
-        std::string m_name;
+        std::string m_label;
     };
 
+    //每次新写一个UI时，写一个DialogDirector子类就可以,其它的组件Button,ListBox之类可以重用
     class FontDialogDirector : public DialogDirector
     {
     public:
-        FontDialogDirector()
-        {
-        }
         virtual ~FontDialogDirector(){}
-        virtual void widgetChanged(Ptr_Widget theChangedWidget)
+        virtual void widgetChanged(Ptr_Widget theChangedWidget) override
         {
             if (theChangedWidget == this->m_fontList)
             {
@@ -162,53 +142,42 @@ namespace MediatorPattern
             }
             else if (theChangedWidget == this->m_ok)
             {
-                if (this->m_fontName->getText() == "")
+                if (this->m_fontName->getText().empty())
                 {
-                    std::cout << "当前未选中任何字体" << std::endl;
+                    std::cout << "请先选中一种字体" << std::endl;
                     return;
                 }
-                //apply font change and dismiss dialog
-                //...
-                std::cout << std::string("the font set to ") + this->m_fontName->getText() + "\n";
-                this->showDialog(false);
+                std::cout << "当前字体设置为:" << this->m_fontName->getText() << std::endl;
+                //dismiss dialog
             }
             else if (theChangedWidget == this->m_cancel)
             {
-                std::cout << "dialog will be hide" << std::endl;
                 //dismiss dialog
-                this->showDialog(false);
             }
         }
-
-        void userInput()
+        virtual void createWidget() override
         {
-            //选择字体列表
-
-            this->m_ok->handleMouse(nullptr);
-
-            std::cout << "====================================" << std::endl;
-            this->m_fontList->handleMouse(nullptr);
-
-            this->m_ok->handleMouse(nullptr);
+            this->m_ok          = std::make_shared<Button>(this->shared_from_this());
+            this->m_ok->setText("ok");
+            this->m_cancel      = std::make_shared<Button>(this->shared_from_this());
+            this->m_cancel->setText("cancel");
+            this->m_fontList    = std::make_shared<ListBox>(this->shared_from_this());
+            std::vector<std::string> fontList = { "consolas", "仿宋", "黑体", "宋体", "微软雅黑" };
+            this->m_fontList->setList(fontList);
+            this->m_fontName    = std::make_shared<EntityField>(this->shared_from_this());
         }
-        virtual void createWidgets() override
-        {
-            auto it = this->shared_from_this();
-            this->m_ok = std::make_shared<Button>(this->shared_from_this());
-            this->m_cancel = std::make_shared<Button>(this->shared_from_this());
-            this->m_fontList = std::make_shared<ListBox>(this->shared_from_this());
-            this->m_fontName = std::make_shared<EntryField>(this->shared_from_this());
 
-            //fill the listBox with the available font name
-            std::vector<std::string> fontName = {"宋体", "微软雅黑", "Consolas", "黑体"};
-            this->m_fontList->setList(fontName);
-            //assemble the widgets in the dialog
+        void run()
+        {
+            this->createWidget();
+            this->m_fontList->handleMouse();
+            this->m_ok->handleMouse();
         }
     private:
-        Ptr_Button m_ok;
-        Ptr_Button m_cancel;
-        Ptr_ListBox m_fontList;
-        Ptr_EntryField m_fontName;
+        Ptr_Button          m_ok;
+        Ptr_Button          m_cancel;
+        Ptr_ListBox         m_fontList;
+        Ptr_EntityField     m_fontName;
     };
 
     class Client
@@ -216,9 +185,8 @@ namespace MediatorPattern
     public:
         static void main()
         {
-            std::shared_ptr<FontDialogDirector> fontDialogDirector = std::make_shared<FontDialogDirector>();
-            fontDialogDirector->createWidgets();
-            fontDialogDirector->userInput();
+            Ptr_FontDialogDirector fontDialogDirector = std::make_shared<FontDialogDirector>();
+            fontDialogDirector->run();
         }
     };
 }
